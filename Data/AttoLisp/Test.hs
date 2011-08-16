6c9d252fb7b03679fdfe20688587491afdd2bdbb
Eric Kow <eric.kow@gmail.com>
2011-08-16 15:27:03 +0100

    Use HUnit to reduce eyeball-testing.

--- Data/AttoLisp/Test.hs
+++ Data/AttoLisp/Test.hs
@@ -9,6 +9,8 @@ import Data.AttoLisp
 import qualified Data.Attoparsec as A
 import qualified Data.Text as T
 import qualified Data.Text.Encoding as T
+import qualified Data.ByteString as B
+import Test.HUnit
 
 data Msg = Msg T.Text Integer
   deriving (Eq, Show)
@@ -27,11 +29,24 @@ test_msg1 = toLisp (Msg "foo" 42)
 test_msg2 = List [Symbol "msg"]
 test_msg3 = List [Symbol "msg", "bar", "baz"]
 
+data T = T { tin  :: B.ByteString
+           , tout :: Either String Lisp
+           }
+
 test_parse :: IO ()
 test_parse = do
-  mapM_ (\inp ->
-           putStrLn $ show inp ++ " => " ++ show (A.parseOnly (lisp <* A.endOfInput) inp))
+  mapM_ (\inp -> do
+           let out = A.parseOnly (lisp <* A.endOfInput) (tin inp)
+           putStrLn $ show (tin inp) ++ " => " ++ show out 
+           assertEqual (show (tin inp)) (tout inp) out
+        )
     inputs
  where
-  inputs = ["()", "42", "(4 5 6)", "(3 (4))", "(3(4))",
-            "\"foo\"", "foo", "(foo \"bar\" 23)"]
+  inputs = [ T "()" (Right $ List [])
+           , T "42" (Right $ Number 42)
+           , T "(4 5 6)" (Right $ List [Number 4, Number 5, Number 6])
+           , T "(3 (4))" (Right $ List [Number 3, List [Number 4]])
+           , T "\"foo\"" (Right (String "foo"))
+           , T "foo"     (Right (Symbol "foo"))
+           , T "(foo \"bar\" 23)" (Right $ List [Symbol "foo", String "bar", Number 23])
+           ]
