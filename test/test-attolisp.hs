3d1b160c7f7fcb75f01da97b46397c241ef6cd80
Eric Kow <eric.kow@gmail.com>
2012-06-18 11:21:08 +0100

    Anonymise parse failures in test suite
    We only care if something fails, not what the error message is.

--- test/test-attolisp.hs
+++ test/test-attolisp.hs
@@ -33,28 +33,33 @@ test_msg2 = List [Symbol "msg"]
 test_msg3 = List [Symbol "msg", "bar", "baz"]
 
 data T = T { tin  :: B.ByteString
-           , tout :: Either String Lisp
+           , tout :: Maybe Lisp
            }
 
 main :: IO ()
 main = defaultMain (map tcase tests)
 
 tcase :: T -> Test.Framework.Test
-tcase (T inp out) = testCase inpStr $ assertEqual inpStr out out2
+tcase (T inp out) = testCase inpStr $ assertParse inpStr out out2
  where
   inpStr = BC.unpack inp
   out2 = A.parseOnly (lisp <* A.endOfInput) inp
 
-tests = [ T "()" (Right $ List [])
-        , T "42" (Right $ Number 42)
-        , T ";;foo\n42" (Right $ Number 42)
-        , T ";;foo\n;;bar\n42" (Right $ Number 42)
-        , T "(4 5 6)" (Right $ List [Number 4, Number 5, Number 6])
-        , T "(4 5 6 )" (Right $ List [Number 4, Number 5, Number 6])
-        , T "(3 (4))" (Right $ List [Number 3, List [Number 4]])
-        , T "'(3 4)" (Right $ List [Symbol "quote", List [Number 3, Number 4]])
-        , T "\"a; however, b\"" (Right $ String "a; however, b")
-        , T "(x ;comment\ny)" (Right $ List [Symbol "x", Symbol "y"])        , T "\"foo\"" (Right (String "foo"))
-        , T "foo"     (Right (Symbol "foo"))
-        , T "(foo \"bar\" 23)" (Right $ List [Symbol "foo", String "bar", Number 23])
+assertParse _ Nothing (Left _) = return ()
+assertParse _ Nothing (Right v2) = assertFailure $ "expected parse error\n but got: " ++ show v2
+assertParse _ (Just v)  (Left e) = assertFailure $ "expected succesful parse: " ++ show v ++ "\n but got error: " ++ e
+assertParse desc (Just v) (Right v2) = assertEqual desc v v2
+
+tests = [ T "()" (Just $ List [])
+        , T "42" (Just $ Number 42)
+        , T ";;foo\n42" (Just $ Number 42)
+        , T ";;foo\n;;bar\n42" (Just $ Number 42)
+        , T "(4 5 6)" (Just $ List [Number 4, Number 5, Number 6])
+        , T "(4 5 6 )" (Just $ List [Number 4, Number 5, Number 6])
+        , T "(3 (4))" (Just $ List [Number 3, List [Number 4]])
+        , T "'(3 4)" (Just $ List [Symbol "quote", List [Number 3, Number 4]])
+        , T "\"a; however, b\"" (Just $ String "a; however, b")
+        , T "(x ;comment\ny)" (Just $ List [Symbol "x", Symbol "y"])        , T "\"foo\"" (Just (String "foo"))
+        , T "foo"     (Just (Symbol "foo"))
+        , T "(foo \"bar\" 23)" (Just $ List [Symbol "foo", String "bar", Number 23])
         ]
